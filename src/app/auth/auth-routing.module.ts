3f49130d0b0f14e6da69faf7db1b70e6a3079c6f
Fabian Jimenez <jose2181685@correo.uis.edu.co>
2023-03-14 19:17:21 -0500

    Login & Registro
    Se desarrollo navbar y footer

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../laboratorio/pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 
 const routes: Routes=[
   {
     path: '',
+    component: HomeComponent,
     children:[
       {
         path:'login',
